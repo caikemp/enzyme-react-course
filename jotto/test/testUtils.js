00d884e76382c3c96230023c6ed61f1984bf2596
Caike Moura Pinheiro <caike.pinheiro@ab-inbev.com>
2020-07-02 09:13:59 -0300

    Add redux thunk

--- jotto/test/testUtils.js
+++ jotto/test/testUtils.js
@@ -1,13 +1,16 @@
 import checkPropTypes from "check-prop-types";
-import { createStore } from "redux";
+import { applyMiddleware, createStore } from "redux";
+import { middlewares } from "../src/configureStore";
 import rootReducer from "../src/reducers";
-
 /**
  *
  * @param {Store} initialState
  */
 export const storeFactory = (initialState = {}) => {
-  return createStore(rootReducer, initialState);
+  const createStoreWithMiddleware = applyMiddleware(...middlewares)(
+    createStore
+  );
+  return createStoreWithMiddleware(rootReducer, initialState);
 };
 
 export const checkProps = (component, conformingProps) => {
