fc8df2a2423621cc2c76c3e70c0950048791c1d9
Caike Moura Pinheiro <caike.pinheiro@ab-inbev.com>
2020-07-03 09:17:13 -0300

    Add Thunk for guessedWrod piece of state

--- jotto/src/reducers/index.js
+++ jotto/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import guessedWords from "./guessedWordsReducer";
+import secretWord from "./secretWordReducer";
 import success from "./successRedux";
 
 export default combineReducers({
   success,
+  secretWord,
   guessedWords,
 });
