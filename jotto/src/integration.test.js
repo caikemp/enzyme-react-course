2e583a3929847c4bd811a83e89efdca8eeefae0b
Caike Moura Pinheiro <caike.pinheiro@ab-inbev.com>
2020-07-03 08:38:29 -0300

    add guessed word reducer

---
+++ jotto/src/integration.test.js
@@ -0,0 +1,15 @@
+import { storeFactory } from '../test/testUtils';
+import {guessWord} from   './actions'
+
+
+describe('guessWord action dispatcher', () => {
+    describe('no guessed words', () => {
+        test('updata state correctly for unsuccessful guess',()=>{})
+        test('updata state correctly for successful guess',()=>{})
+    })
+
+    describe('some guessed words', () => {
+        test('updata state correctly for unsuccessful guess',()=>{})
+        test('updata state correctly for successful guess',()=>{})
+    })
+})
--- jotto/src/reducers/index.js
+++ jotto/src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
+import guessedWords from "./guessedWordsReducer";
 import success from "./successRedux";
 
 export default combineReducers({
   success,
+  guessedWords,
 });
