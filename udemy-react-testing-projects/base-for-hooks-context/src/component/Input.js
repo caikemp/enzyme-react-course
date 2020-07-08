e24f297d74b9bbefa6710a75958f7a5ed651f49a
Caike Moura Pinheiro <caike.pinheiro@ab-inbev.com>
2020-07-08 09:03:46 -0300

    Add props to input

--- udemy-react-testing-projects/base-for-hooks-context/src/component/Input.js
+++ udemy-react-testing-projects/base-for-hooks-context/src/component/Input.js
@@ -1,7 +1,12 @@
+import PropTypes from "prop-types";
 import React from "react";
 
-const Input = () => {
+const Input = ({ secretWord }) => {
   return <div data-test="component-input"></div>;
+};
+
+Input.propTypes = {
+  secretWord: PropTypes.string.isRequired,
 };
 
 export default Input;
--- udemy-react-testing-projects/base-for-hooks-context/src/component/Input.test.js
+++ udemy-react-testing-projects/base-for-hooks-context/src/component/Input.test.js
@@ -1,14 +1,18 @@
 import { shallow } from "enzyme";
 import React from "react";
-import { findByTestAttr } from "../../test/testUtils";
+import { checkProps, findByTestAttr } from "../../test/testUtils";
 import Input from "./Input";
 
-const setup = () => {
-  return shallow(<Input />);
+const setup = (secretWord = "party") => {
+  return shallow(<Input secretWord={secretWord} />);
 };
 
 test("Input renders without error", () => {
   const wrapper = setup();
   const component = findByTestAttr(wrapper, "component-input");
   expect(component.length).toBe(1);
+});
+
+test("does not throw warning with expected props", () => {
+  checkProps(Input, { secretWord: "party" });
 });
