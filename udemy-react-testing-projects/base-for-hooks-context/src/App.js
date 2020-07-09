df0f36d09537faf0f1f70f4b28a48e0171486596
Caike Moura Pinheiro <caike.pinheiro@ab-inbev.com>
2020-07-09 09:26:05 -0300

    Add tests to app

--- udemy-react-testing-projects/base-for-hooks-context/src/App.js
+++ udemy-react-testing-projects/base-for-hooks-context/src/App.js
@@ -1,7 +1,35 @@
 import React from "react";
+import hookActions from "./actions/hookActions";
 import "./App.css";
 
+/**
+ *
+ * @param {object} state
+ * @param {object} action contain 'type' and 'payload' properties for the state update
+ * @return {object} - new state
+ */
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "setSecretWord":
+      return {
+        ...state,
+        secretWord: action.payload,
+      };
+
+    default:
+      return state;
+  }
+};
+
 function App() {
+  const [state, dispatch] = React.useReducer(reducer, { secretWord: "" });
+
+  const setSecretWord = (secretWord) =>
+    dispatch({ type: "setSecretWord", payload: secretWord });
+
+  React.useEffect(() => {
+    hookActions.getSecretWord(setSecretWord);
+  }, []);
   return <div data-test="component-app" />;
 }
 
--- udemy-react-testing-projects/base-for-hooks-context/src/App.test.js
+++ udemy-react-testing-projects/base-for-hooks-context/src/App.test.js
@@ -1,14 +1,36 @@
-import { shallow } from "enzyme";
+import { mount } from "enzyme";
 import React from "react";
 import { findByTestAttr } from "../test/testUtils";
+import hookActions from "./actions/hookActions";
 import App from "./App";
 
+const mockGetSecretWord = jest.fn();
+
 const setup = () => {
-  return shallow(<App />);
+  mockGetSecretWord.mockClear();
+  hookActions.getSecretWord = mockGetSecretWord;
+
+  return mount(<App />);
 };
 
 test("App renders without error", () => {
   const wrapper = setup();
   const component = findByTestAttr(wrapper, "component-app");
   expect(component.length).toBe(1);
+});
+
+describe("getSecretWord calls", () => {
+  test("getSecretWord gets called on App mount", () => {
+    setup();
+
+    expect(mockGetSecretWord).toHaveBeenCalled();
+  });
+
+  test("secretWord does not update on App update", () => {
+    const wrapper = setup();
+    mockGetSecretWord.mockClear();
+    wrapper.setProps();
+
+    expect(mockGetSecretWord).not.toHaveBeenCalled();
+  });
 });
